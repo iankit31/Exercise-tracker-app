a58dda0e98c0a8287176ac492bf6d79596b74e20
Ankit <imankitpandey31@gmail.com>
2021-07-20 00:32:20 +0530

    added function to create and delete user and exercise

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,76 +1,88 @@
-import React,{Component} from 'react'
-// import axios from 'axios';
+import React, { Component } from 'react';
+import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
-export default class CreateExercise extends React.Component { 
+export default class CreateExercise extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            username:  '',
-            description: '',
-            duration: 0,
-            date: new Date(),
-            users: []
-         }
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
-        this.onChangeUsername = this.onChangeUsername.bind(this);
-        this.onChangeDescription = this.onChangeDescription.bind(this);
-        this.onChangeDuration = this.onChangeDuration.bind(this);
-        this.onChangeDate = this.onChangeDate.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
+    this.state = {
+      username: '',
+      description: '',
+      duration: 0,
+      date: new Date(),
+      users: []
     }
+  }
 
-    componentDidMount() {
-        this.setState({
-            users: ['test user'],
-            username: 'test user',
-        })
-    }
-
-    onChangeUsername(e) {
-        this.setState({
-          username: e.target.value
-        })
-      }
-    
-      onChangeDescription(e) {
-        this.setState({
-          description: e.target.value
-        })
-      }
-    
-      onChangeDuration(e) {
-        this.setState({
-          duration: e.target.value
-        })
-      }
-    
-      onChangeDate(date) {
-        this.setState({
-          date: date
-        })
-      }
-
-      onSubmit(e) {
-        e.preventDefault();
-       
-        const exercise = {
-            username: this.state.username,
-            description: this.state.description,
-            duration: this.state.duration,
-            date: this.state.date
+  componentDidMount() {
+    axios.get('http://localhost:5000/users/')
+      .then(response => {
+        if (response.data.length > 0) {
+          this.setState({
+            users: response.data.map(user => user.username),
+            username: response.data[0].username
+          })
         }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
 
-        console.log(exercise);
+  }
 
-        window.location = '/';
-      }
-    render() {
-        return (
-            <div>
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    })
+  }
+
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value
+    })
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date
+    }
+
+    console.log(exercise);
+
+    axios.post('http://localhost:5000/exercises/add', exercise)
+      .then(res => console.log(res.data));
+
+    window.location = '/';
+  }
+
+  render() {
+    return (
+    <div>
       <h3>Create New Exercise Log</h3>
       <form onSubmit={this.onSubmit}>
         <div className="form-group"> 
@@ -117,12 +129,12 @@ export default class CreateExercise extends React.Component {
             />
           </div>
         </div>
-        <br />
+          <br/>
         <div className="form-group">
           <input type="submit" value="Create Exercise Log" className="btn btn-primary" />
         </div>
       </form>
     </div>
-        )
-    }
+    )
+  }
 }
