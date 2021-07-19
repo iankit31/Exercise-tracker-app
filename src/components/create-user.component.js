4851b9fed0a4b3a37a972f5570d8918a7ed4d467
Ankit <imankitpandey31@gmail.com>
2021-07-19 23:24:47 +0530

    creating different componets

---
+++ src/components/create-user.component.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+
+export default class CreateUser extends Component{ 
+    render() {
+        return (
+            <div>
+                <h2>in Create User</h2>
+            </div>
+        )
+    }
+}
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+
+export default class ExercisesList extends Component{ 
+    render() {
+        return (
+            <div>
+                <h2>Exercises List</h2>
+            </div>
+        )
+    }
+}
